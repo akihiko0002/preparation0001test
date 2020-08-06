53c7c5d0b9b964394b93120af0f9b24c8e76b651
Akihiko Fukuda <ainic0107@gmail.com>
2020-08-06 23:59:49 +0900

    app更新

--- preparationtest/app.py
+++ preparationtest/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +8,11 @@ def helloworld():
 @app.route("/<name>")
 def greet(name):
     return name + "さん、はろー！"
+
+@app.route("/template")
+def template():
+    py_name="akihiko"
+    return render_template("index.html", name =py_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
