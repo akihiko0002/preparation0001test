b7510254eabd32df70a8e0c21caf3a9437fc6da7
Akihiko Fukuda <ainic0107@gmail.com>
2020-08-06 23:34:07 +0900

    app

---
+++ preparationtest/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def helloworld():
+    return "Hello World."
+
+@app.route("/<name>")
+def greet(name):
+    return name + "さん、はろー！"
+
+if __name__ == "__main__":
+    app.run(debug=True)
